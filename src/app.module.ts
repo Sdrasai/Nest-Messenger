7f2de6b92a266da3b15249e85c185cc17db6d469
amirneon <amirneon7984@gmail.com>
2024-04-03 00:11:21 +0330

    Sadra

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,40 @@
-import { Module } from '@nestjs/common'
-import { MongooseModule } from '@nestjs/mongoose'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { UsersModule } from './users/users.module'
-import { AuthModule } from './auth/auth.module'
-import { APP_GUARD } from '@nestjs/core'
-import { AuthGuard } from './common/guards/auth.guard'
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { getConnectionToken, MongooseModule } from "@nestjs/mongoose";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { AuthModule } from "./auth/auth.module";
+import { APP_GUARD } from "@nestjs/core";
+import { AuthGuard } from "./common/guards/auth.guard";
 
-import { MessagesModule } from './messages/messages.module'
+import { MessagesModule } from "./messages/messages.module";
+import { SocketAuthGuardMiddleware } from "./middlewares/messenger.middleware";
+// import { Message, messageSchema } from "./messages/entities/message.entity";
+// import { Connection } from "mongoose";
+// import * as AutoIncrementFactory from "mongoose-sequence";
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
-    MongooseModule.forRoot('mongodb://mongodb:27017/Messenger'),
+    MongooseModule.forRoot("mongodb://mongodb:27017/Messenger"),
+    // MongooseModule.forFeatureAsync([
+    //   {
+    //     name: Message.name,
+    //     useFactory: async (connection: Connection) => {
+    //       const schema = messageSchema;
+    //       const AutoIncrement = AutoIncrementFactory(connection);
+    //       schema.plugin(AutoIncrement, { inc_field: "id" });
+    //       return schema;
+    //     },
+    //     inject: [getConnectionToken("mongodb://mongodb:27017/Messenger")],
+    //   },
+    // ]),
     MessagesModule,
   ],
   controllers: [AppController],
   providers: [
+    // MessagesGateway,
     AppService,
     {
       provide: APP_GUARD,
@@ -25,4 +42,9 @@ import { MessagesModule } from './messages/messages.module'
     },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(SocketAuthGuardMiddleware).forRoutes("api/v1/chat");
+  }
+}
+// export class AppModule {}
