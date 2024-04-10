7d645642f24343bef75bbab48a72ef0a31bfeae4
amirneon <amirneon7984@gmail.com>
2024-04-10 21:36:21 +0330

    InnerFix

--- dist/common/decorators/public.decorators.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Public = void 0;
-const common_1 = require("@nestjs/common");
-const auth_constants_1 = require("../constants/auth.constants");
-const Public = () => (0, common_1.SetMetadata)(auth_constants_1.IS_PUBLIC_KEY, true);
-exports.Public = Public;
-//# sourceMappingURL=public.decorators.js.map
